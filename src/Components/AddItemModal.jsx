8a441c909941c1995287c4cc13c745da9546dcde
glazialuna <glaziawy@gmail.com>
2022-11-09 12:48:14 -0600

    modify the additem; fix the additem submit error

--- src/Components/AddItemModal.jsx
+++ src/Components/AddItemModal.jsx
@@ -39,6 +39,7 @@ const validateInput = (quantity, volume) => {
 
   const AddItemModal = ({count, uid}) => {
     const [update, result] = useDbUpdate(`/Products/${uid}`);
+    const [products, loading, error] = useData(`/Products/${uid}`);
     //const [state, change] = useFormData(validateInput, product);
     const [state, change] = useState({values:{category: "", img_url: "", name: "", quantity: 0, volume: 0}});
 
@@ -46,12 +47,14 @@ const validateInput = (quantity, volume) => {
       evt.preventDefault();
       const vol = document.getElementById("volume_new").value;
       const catalog = document.getElementById("category_new").value;
-      
+      const img = document.getElementById("img_new").value;
+      count = Object.entries(products).length
+      console.log(count);
       // const quantity = document.getElementById("quantity").value;
       const jsonObj = {
         [count]: {
           category: catalog,
-          img_url: "",
+          img_url: img,
           name: count,
           quantity: 0,
           volume: vol
@@ -71,8 +74,16 @@ const validateInput = (quantity, volume) => {
                 <InputField name="category" text="Category" state={state} change={change} /> */}
                   Volume (mL):
                   <input id="volume_new" name="volume" text="Volume (mL)"  />
+                  <br/>
                   Category:
                   <input id="category_new" name ="category" text="Category" />
+                  <br/>
+                  Image Url:
+                  <input id="img_new" name ="img url" text="image url" />
+                  {document.getElementById("img_new")?
+                  <img src={document.getElementById("img_new").value} style={{marginTop: "4px", height: "100px"}} alt="new"></img>
+                  :""}
+                  <br/>
                   <button  
                     type="submit"
                     value="Submit"
